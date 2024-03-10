3df92aa208428276f903acc5d9acbefe58cd4595
ChirayuBorawake <borawakechirayu3@gmail.com>
2024-03-10 22:16:15 +0530

    initial commit

---
+++ springbootawscore/src/main/java/com/csi/SpringbootawscoreApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootawscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootawscoreApplication.class, args);
+	}
+
+}
